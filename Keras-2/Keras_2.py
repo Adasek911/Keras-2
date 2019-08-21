3c805999a5020817b8b35904b61621f8887cb173
Adasek911 <adam.nowacki@hs-bochum.de>
2019-08-21 08:54:41 +0200

    Richtig

--- Keras-2/Keras_2.py
+++ Keras-2/Keras_2.py
@@ -5,25 +5,25 @@ from keras.utils import np_utils
 from keras.datasets import mnist
 #https://www.youtube.com/watch?v=KoINpCtykHc
 
-(XTrain, YTrain),(XTest, YTest) = mnist.load_data()
+(XTrain, yTrain),(XTest, yTest) = mnist.load_data()
 
 XTrain = XTrain.reshape(60000, 784)
 XTest = XTest.reshape(10000, 784)
 XTrain = XTrain/255
 XTest = XTest/255
 
-YTrain = np_utils.to_categorical(YTrain, 10)
-YTest = np_utils.to_categorical(YTest, 10)
+YTrain = np_utils.to_categorical(yTrain, 10)
+YTest = np_utils.to_categorical(yTest, 10)
 
 myANN = Sequential()
 myANN.add(Dense(80,input_dim=784,activation="relu"))
 myANN.add(Dense(40,activation="relu"))
 myANN.add(Dense(10,activation="sigmoid"))
 myANN.compile(loss="mean_squared_error", optimizer="adam", metrics= ["accuracy"])
-myANN.fit(XTrain, YTrain, epochs=8, verbose=True)
+myANN.fit(XTrain, YTrain, epochs=5, verbose=True)
 
 yP = myANN.predict(XTest)
-diffCases = np.flatnonzero(np.argmax(yP,axis=1)-YTest)
+diffCases = np.flatnonzero(np.argmax(yP,axis=1)-yTest)
 print("Richtig: %.2f%%  " % (100 - diffCases.shape[0]/100))
 
 
